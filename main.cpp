2ccd0d7c8ef97a38d61bc6b16623409a20ae7572
mzainiulilabshor <45529539+mzainiulilabshor@users.noreply.github.com>
2019-01-05 02:06:14 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int r,luas,keliling;
+cout << " Program Menghitung Keliling & Luas Lingkaran " << endl;
+cout << " Masukan Jari-Jari Lingkaran : "; cin>>r;
+
+keliling = 2*3.14*r;
+cout << " Keliling : " << keliling << endl;
+luas=3.14*r*r;
+cout << " Luas : " << luas;
+    return 0;
+}
